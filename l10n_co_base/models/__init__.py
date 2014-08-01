1403b4d509845cd6f78bb876bd11d9c28ca48a51
blaggacao <david@elaleman.co>
2014-08-01 17:11:09 -0500

    apply changes again

--- l10n_co_base/models/__init__.py
+++ l10n_co_base/models/__init__.py
@@ -1,20 +1,26 @@
 # -*- encoding: utf-8 -*-
-###############################################################################
-#                                                                             #
-# Copyright (C) 2014  David Arnold (El Aelman SAS)                            #
-#                                                                             #
-# This program is free software: you can redistribute it and/or modify        #
-# it under the terms of the GNU Affero General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or           #
-# (at your option) any later version.                                         #
-#                                                                             #
-# This program is distributed in the hope that it will be useful,             #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
-# GNU Affero General Public License for more details.                         #
-#                                                                             #
-# You should have received a copy of the GNU Affero General Public License    #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
-###############################################################################
+##############################################################################
+#
+# OpenERP, Open Source Management Solution
+# Copyright (C) David Arnold (devCO).
+# Author        David Arnold (devCO)
+# Co-Authors    Juan Pablo Aries (devCO)
+#               Nhomar Hernandez (Vauxoo)
+#               Humberto Ochoa (Vauxoo)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 import res_partner
