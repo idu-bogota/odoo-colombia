7678a3f6657a03db07408d2c6eea6a75b0baa6f0
blaggacao <david@elaleman.co>
2014-08-08 09:09:34 -0500

    bugfixes

--- res_partner_fiscal_document/res_partner.py
+++ res_partner_fiscal_document/res_partner.py
@@ -33,9 +33,11 @@ class ResPartner(models.Model):
         'res.partner.idtype',
         string=u'Document Type',
         domain=dom,
-        compute='validateformatcopy',
     )
-    fiscal_id = fields.Char(string=u'Document ID')
+    fiscal_id = fields.Char(
+        string=u'Document ID',
+        # compute='validateformatcopy',
+    )
     fiscal_id_doc = fields.Binary(
         string=u'Document Scan',
         help="Upload the supporting Document "
@@ -46,7 +48,7 @@ class ResPartner(models.Model):
     )
 
     @api.one
-    @api.depends(
+    @api.onchange(
         'fiscal_id_type',
         'fiscal_id',
         'is_company',
