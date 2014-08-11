f4cf2ee25e0920ea3d544573b29c55ec1570e3ef
blaggacao <david@elaleman.co>
2014-08-11 02:21:47 -0500

    fine tuning

--- l10n_co_razonsocial/res_partner_razonsocial.py
+++ l10n_co_razonsocial/res_partner_razonsocial.py
@@ -24,35 +24,9 @@ from openerp import models, fields, api, _
 
 
 class ResPartnerRazonsocial(models.Model):
-    _name = 'res.partner'
     _inherit = 'res.partner'
 
-    state = fields.Selection(
-        (('pernat', 'Persona Natural'), ('perjur', 'Persona Juridica')),
-        default='perjur', copy=False)
-    legal_denomination = fields.Char(compute='_copy_values')
-    legal_entity_name = fields.Char(
-        required=True,
-        states={'pernat': [('invisible', True),('required', False)]}
-    )
-    firstname = fields.Char(
-        required=True,
-        states={'perjur': [('invisible', True),('required', False)]}
-    )
-    middlename = fields.Char(
-        required=True,
-        states={'perjur': [('invisible', True),('required', False)]}
-    )
-    first_lastname = fields.Char(
-        required=True,
-        states={'perjur': [('invisible', True),('required', False)]}
-    )
-    second_lastname = fields.Char(
-        required=True,
-        states={'perjur': [('invisible', True,('required', False))]}
-    )
-
-    @api.v8
+    @api.one
     @api.depends(
         'legal_entity_name',
         'firstname',
@@ -61,11 +35,75 @@ class ResPartnerRazonsocial(models.Model):
         'second_lastname'
     )
     def _copy_values(self):
-        if self.state = 'perjur':
-            self.legal_denomination = self.legal_entity_name
-        else:
+        title = self.title.shortcut and ' ' + self.title.shortcut or ''
+        if self.state == 'perjur':
+            name = self.legal_entity_name and self.legal_entity_name or ''
+            self.legal_denomination = name + title
+        elif self.state == 'pernat':
             s1 = self.firstname and self.firstname + ' ' or ''
             s2 = self.middlename and self.middlename + ' ' or ''
             s3 = self.first_lastname and self.first_lastname + ' ' or ''
             s4 = self.second_lastname and self.second_lastname or ''
-            self.legal_denomination = s1 + s2 + s3 + s4
+            self.legal_denomination = s1 + s2 + s3 + s4 + title
+        else:
+            self.legal_denomination = ''
+
+    state = fields.Selection(
+        (
+            ('pernat', 'Persona Natural'),
+            ('perjur', 'Persona Juridica')
+        ),
+        copy=False
+    )
+    legal_denomination = fields.Char(
+        string=u'Legal Name',
+        readonly=True,
+        store=True,
+        compute='_copy_values'
+    )
+    legal_entity_name = fields.Char(
+        states={
+            'perjur': [('required', True)],
+            'pernat': [('invisible', True)],
+            False: [('invisible', True)]
+        }
+    )
+    firstname = fields.Char(
+        states={
+            'pernat': [('required', True)],
+            'perjur': [('invisible', True)],
+            False: [('invisible', True)]
+        }
+    )
+    middlename = fields.Char(
+        states={
+            'perjur': [('invisible', True)],
+            False: [('invisible', True)]
+        }
+    )
+    first_lastname = fields.Char(
+        states={
+            'pernat': [('required', True)],
+            'perjur': [('invisible', True)],
+            False: [('invisible', True)]
+        }
+    )
+    second_lastname = fields.Char(
+        states={
+            'perjur': [('invisible', True)],
+            False: [('invisible', True)]
+        }
+    )
+
+    # Override from res.partner
+    @api.multi
+    def onchange_type(self, is_company):
+        value = {}
+        value['title'] = False
+        if is_company:
+            value['use_parent_address'] = False
+            domain = {'title': [('domain', '=', 'partner')]}
+        else:
+            domain = {'title': [('domain', '=', 'contact')]}
+            value['state'] = False
+        return {'value': value, 'domain': domain}
